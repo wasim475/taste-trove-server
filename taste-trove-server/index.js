769c9276a8d5fc1133b13995ecbd0c447302e6e8
wasim475 <wasimhossain575@gmail.com>
2024-05-15 06:55:57 +0600

    added delete part on database

--- taste-trove-server/index.js
+++ taste-trove-server/index.js
@@ -59,6 +59,12 @@ async function run() {
       res.send(result);
   })
 
+  app.delete('/purchases/:id',async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)}
+    const result = await PurchasesDataCollection.deleteOne(query)
+    res.send(result)
+  })
   app.delete('/foods/:id',async(req, res)=>{
     const id = req.params.id;
     const query = {_id: new ObjectId(id)}
